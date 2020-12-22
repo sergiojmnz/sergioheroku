9c8dbe3845fced459a27a32a92725a900e657010
sergiojmnz <61261358+sergiojmnz@users.noreply.github.com>
2020-12-21 18:05:42 -0600

    Update QuestionController.java

--- src/main/java/com/example/postgresdemo/controller/QuestionController.java
+++ src/main/java/com/example/postgresdemo/controller/QuestionController.java
@@ -30,7 +30,7 @@ public class QuestionController {
 
     @PutMapping("/questions/{questionId}")
     public Question updateQuestion(@PathVariable Long questionId,
-                                   @Valid@RequestBody Question questionRequest) {
+                                   @RequestBody Question questionRequest) {
         return questionRepository.findById(questionId)
                 .map(question -> {
                     question.setTitle(questionRequest.getTitle());
